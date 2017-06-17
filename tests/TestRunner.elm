2b5982803a5fab8e2dac668f73c11f0337f8e42d
Avinash <avinash@avinash.com.np>
2017-06-17 10:53:34 +0545

    convert ad2bs function added, tests added

--- tests/TestRunner.elm
+++ tests/TestRunner.elm
@@ -1,25 +1,34 @@
 module TestRunner exposing (..)
 
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, list, int, string)
-import Test exposing (..)
 import Adbs exposing (ad2bs, miti)
+import Expect exposing (Expectation)
+import Test exposing (..)
 
 
 suite : Test
 suite =
-    describe "Sample Test Suite"
-        [ describe "Unit test examples"
-            [ test "Addition" <|
-                \() ->
-                    Expect.equal (3 + 7) 10
-            ]
-        , describe "Ad2BS examples"
+    describe "Adbs Tests"
+        [ describe "Converting AD dates to BS tests"
             [ test "0BC should fail" <|
                 \() ->
                     Expect.equal (ad2bs "0/0/0") Nothing
-            , test "date should work" <|
+            , test "Invalid Dates should fail" <|
                 \() ->
-                    Expect.equal (ad2bs "2016/10/11") (Just (miti 2016 10 11))
+                    Expect.equal (ad2bs "2000/02/32") Nothing
+            , test "2000/02/02 - 2056/10/19" <|
+                \() ->
+                    Expect.equal (ad2bs "2000/02/02") (Just (miti 2056 10 19))
+            , test "2017/06/17 - 2074/03/03" <|
+                \() ->
+                    Expect.equal (ad2bs "2017/06/17") (Just (miti 2074 3 3))
+            , test "2014/01/01 - 2070/9/17" <|
+                \() ->
+                    Expect.equal (ad2bs "2014/01/01") (Just (miti 2070 9 17))
+            , test "2015/7/19 - 2072/4/3'" <|
+                \() ->
+                    Expect.equal (ad2bs "2015/07/19") (Just (miti 2072 4 3))
+            , test "2015/7/17 - 2072/4/1" <|
+                \() ->
+                    Expect.equal (ad2bs "2015/07/17") (Just (miti 2072 4 1))
             ]
         ]
